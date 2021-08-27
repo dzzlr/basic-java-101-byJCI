0968db7d7cface6774e5bc2ffa1cf4babae46b58
Fakhri Hassan Maulana <87844859+dzzlr@users.noreply.github.com>
2021-08-27 17:15:31 +0700

    just commit

--- mod4_array/array/src/array/Array.java
+++ mod4_array/array/src/array/Array.java
@@ -1,20 +1,10 @@
 package array;
-import java.util.ArrayList;
-
 
 public class Array {
     public static void main(String[] args) {
         String[] car = {"BMW", "Mazda", "Mercedes", "Lexus", "Porsche"};
         for (int i = 0; i < car.length; i++) {
             System.out.println("ini adalah car ke-" + i + " = "+ car[i]);
-        }
-        
-        ArrayList<String> food = new ArrayList<String>();
-        food.add("Croffle");
-        food.add("Cheese Cake");
-        food.add("Taiyaki");
-        food.add("Klepon");
-        food.add("Surabi");
-        System.out.println(food);
+        }        
     } 
 }
